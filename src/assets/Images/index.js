183682113f8903c403cdc2e64a4b3ee16c89deec
whitemousess <thangcoiofficial@gmail.com>
2023-09-06 23:05:18 +0700

    a

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,5 +1,5 @@
 const images = {
-    logo: require('~/assets/images/logo.svg').default,
+    logo: require('~/assets/Images/logo.svg').default,
     avatarDefault: require('~/assets/images/image-default.png')
 };
 
