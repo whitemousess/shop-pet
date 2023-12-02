60998676088e1bc61f2c9f0947a535dd73683061
whitemousess <thangcoiofficial@gmail.com>
2023-12-02 20:43:09 +0700

    scroll

--- src/layouts/components/Header/Menu/Menu.js
+++ src/layouts/components/Header/Menu/Menu.js
@@ -4,53 +4,77 @@ import { Link } from "react-router-dom";
 
 import config from "~/config";
 import styles from "./Menu.module.scss";
+import { useEffect, useState } from "react";
 
 const cx = classNames.bind(styles);
 
 function Menu() {
+const [isSticky,setIsSticky] = useState(false)
+
+useEffect(() => {
+  const handleScroll = () => {
+    const navbar = document.getElementById('header');
+    const navbarHeight = navbar.offsetHeight;
+    const currentScroll = window.scrollY;
+    console.log(navbarHeight)
+
+    if (currentScroll > 160) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
   return (
-    <div className={cx("wrapper")}>
-      <Tippy
-        interactive
-        delay={[0, 0]}
-        offset={[-20, 0]}
-        placement={"bottom"}
-      >
-        <Link className={cx("link")} to={config.routes.dogs}>
-          CHÓ
-        </Link>
-      </Tippy>
-      <Tippy
-        interactive
-        delay={[0, 0]}
-        offset={[-20, 0]}
-        placement={"bottom"}
-      >
-        <Link className={cx("link")} to={config.routes.cats}>
-          MÈO
-        </Link>
-      </Tippy>
-      <Tippy
-        interactive
-        delay={[0, 0]}
-        offset={[-20, 0]}
-        placement={"bottom"}
-      >
-        <Link className={cx("link")} to={config.routes.itemDogs}>
-          ĐỒ CHO CHÓ
-        </Link>
-      </Tippy>
-      <Tippy
-        interactive
-        delay={[0, 0]}
-        offset={[-20, 0]}
-        placement={"bottom"}
-      >
-        <Link className={cx("link")} to={config.routes.itemCats}>
-          ĐỒ CHO MÈO
-        </Link>
-      </Tippy>
-    </div>
+    <div className={cx("wrapper", { "scroll": isSticky })} id="header">
+        <Tippy
+          interactive
+          delay={[0, 0]}
+          offset={[-20, 0]}
+          placement={"bottom"}
+        >
+          <Link className={cx("link")} to={config.routes.dogs}>
+            CHÓ
+          </Link>
+        </Tippy>
+        <Tippy
+          interactive
+          delay={[0, 0]}
+          offset={[-20, 0]}
+          placement={"bottom"}
+        >
+          <Link className={cx("link")} to={config.routes.cats}>
+            MÈO
+          </Link>
+        </Tippy>
+        <Tippy
+          interactive
+          delay={[0, 0]}
+          offset={[-20, 0]}
+          placement={"bottom"}
+        >
+          <Link className={cx("link")} to={config.routes.itemDogs}>
+            ĐỒ CHO CHÓ
+          </Link>
+        </Tippy>
+        <Tippy
+          interactive
+          delay={[0, 0]}
+          offset={[-20, 0]}
+          placement={"bottom"}
+        >
+          <Link className={cx("link")} to={config.routes.itemCats}>
+            ĐỒ CHO MÈO
+          </Link>
+        </Tippy>
+      </div>
   );
 }
 
